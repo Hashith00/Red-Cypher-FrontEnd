f8ce8af7efec61a8f7d3b06635e77f367aac6b3b
Hashith Sithuruwan <mailtohashith@gmail.com>
2024-04-18 14:18:58 +0530

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [count, setCount] = useState(0);
+  //const [flag, getFlag] = useState("");
+
+  const getIntoHackersDen = async () => {
+    console.log("hello");
+    try {
+      const responce = await axios.get(
+        "http://localhost:3000/api/getFlagUncry"
+      );
+      console.log(responce.data["Flag"]);
+
+      // getFlag(responce.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => getIntoHackersDen()}>Hackers Den</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
