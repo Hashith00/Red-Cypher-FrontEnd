17800776cea94e4d620cb1e558ac48d17192f29c
Hashith Sithuruwan <mailtohashith@gmail.com>
2024-04-19 23:00:19 +0530

    Game Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-import redLogo from "./assets/Red Cypher Font with logo 1.0.png";
-import redSmallLogo from "./assets/Red Cypher Logo - white removed.png";
 import "./App.css";
 import axios from "axios";
 import DownloadButton from "./components/downloadButton.jsx";
@@ -10,11 +8,44 @@ function App() {
   const [count, setCount] = useState(0);
   //const [flag, getFlag] = useState("");
 
+  const handleDownload = () => {
+    const pdfUrl = "https://244.redcypher.org" + "/data_pdf.pdf";
+
+    console.log(pdfUrl);
+
+    // Create a temporary anchor element
+    const anchor = document.createElement("a");
+    anchor.href = pdfUrl;
+    anchor.download = "data_pdf.pdf";
+
+    // Programmatically trigger a click event on the anchor element to start the download
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  };
+
+  const handleDownloadZip = () => {
+    const pdfUrl =
+      "https://244.redcypher.org" + "/когда_произошел_первый_дефкон.rar";
+
+    console.log(pdfUrl);
+
+    // Create a temporary anchor element
+    const anchor = document.createElement("a");
+    anchor.href = pdfUrl;
+    anchor.download = "когда_произошел_первый_дефкон.rar";
+
+    // Programmatically trigger a click event on the anchor element to start the download
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  };
+
   const getIntoHackersDen = async () => {
     console.log("hello");
     try {
       const responce = await axios.get(
-        "http://localhost:3000/api/getFlagUncry"
+        "https://244.redcypher.org/backend/api/getFlagUncry"
       );
       console.log(responce.data["Flag"]);
 
@@ -28,10 +59,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={redLogo} className="logo" alt="Vite logo" />
+          <img src="/Red_long.png" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={redSmallLogo} className="logo react" alt="React logo" />
+          <img src="/Red_shaort.png" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>LET ME HACK YOU</h1>
@@ -40,10 +71,13 @@ function App() {
           count is {count}
         </button>
         <button onClick={() => getIntoHackersDen()}>Hackers Den</button>
-        <DownloadButton filePath="/assets/data_pdf.pdf" fileName="Data file" />
+        <button onClick={() => handleDownload()}>Hackers Den</button>
+        <button onClick={() => handleDownloadZip()}>Zip</button>
+
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <a href="game.html">Game</a>
       </div>
       <p className="read-the-docs">Click Here</p>
     </>
--- src/components/downloadButton.jsx
+++ src/components/downloadButton.jsx
@@ -2,18 +2,13 @@ import React from "react";
 
 function DownloadButton({ filePath, fileName }) {
   const handleDownload = () => {
-    fetch(filePath)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", fileName);
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-      })
-      .catch((error) => console.error("Error downloading the file: ", error));
+    const pdfUrl = filePath;
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = fileName; // specify the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
   return <button onClick={handleDownload}>Download {fileName}</button>;
 }
